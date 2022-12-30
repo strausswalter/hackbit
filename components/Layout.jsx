8765216b0952ea409711e68447a5da660822555a
strausswalter <strausswalter@gmail.com>
2022-12-30 08:12:38 -0300

    acrescentado arquivo de exemplo .envTEMPLATE

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import Head from 'next/head';
+import React from "react";
+import Head from "next/head";
 
-import Navbar from './Navbar';
-import Footer from './Footer';
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 const Layout = ({ children }) => {
   return (
@@ -13,14 +13,12 @@ const Layout = ({ children }) => {
       <header>
         <Navbar />
       </header>
-      <main className="main-container">
-        {children}
-      </main>
+      <main className="main-container">{children}</main>
       <footer>
         <Footer />
       </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
